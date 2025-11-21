05c11620950a9c385658a92a530644a7699272f3
whcheungav <marcocheung0521@gmail.com>
2025-11-21 09:21:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ col1, col2= st.columns(2)
 
 with coli:
   st.header("Q1 2024")
-    st.write("Revenue: $1.2M")
+  st.write("Revenue: $1.2M")
 with col2:
     st.header("Q2 2024")
     st.write("Revenue: $1.5M")
