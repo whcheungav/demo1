f0a4f268a8222c0d76ed97eba4b330e57755abee
whcheungav <marcocheung0521@gmail.com>
2025-12-15 12:22:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -309,7 +309,7 @@ import matplotlib.pyplot as plt
 st.title("Hello")
 st.write("Hello")
 st.header("Hello")
-st.number_input("Hello",value=36,max_value=1000,min_value=5)
+st.number_input("Hello",1000,0,5)
 input=st.selectbox("Hello",["Hi","Bye","Yo"])
 st.write(f"You have input: {input}")
 
